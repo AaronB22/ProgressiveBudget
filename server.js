f91ca7073ced0034ce0059c68a2399541b3f3de4
Aaron <aaronthesnowboarder@live.com>
2021-07-10 19:13:39 -0700

    bug fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 const PORT = 3000;
 
 const app = express();
@@ -14,8 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/budget", {
+console.log('mongodb+srv://aaronb22:1LazyDog00@cluster0.wozkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
